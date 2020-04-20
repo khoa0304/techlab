75524387379e08c17043c4949254bf6581d1f50e
kntran <kntran@kntran-lap1.oradev.oraclecorp.com>
2020-04-19 22:51:50 -0700

    update security

--- Lab.DiscoveryService/src/main/java/lab/discoveryservice/DiscoveryApplication.java
+++ Lab.DiscoveryService/src/main/java/lab/discoveryservice/DiscoveryApplication.java
@@ -11,4 +11,6 @@ public class DiscoveryApplication {
 	public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
     }
+	
+
 }
--- Lab.DiscoveryService/src/main/java/lab/security/WebSecurity.java
+++ Lab.DiscoveryService/src/main/java/lab/security/WebSecurity.java
@@ -1,20 +1,29 @@
 package lab.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-        		http
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+	
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//			 User.withUsername(username)()
+//				.username("user")
+//				.password("password")
+//				.roles("USER")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
 }
