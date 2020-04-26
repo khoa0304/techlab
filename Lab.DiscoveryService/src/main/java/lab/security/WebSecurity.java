05a43b331d9d342ec1d69147c3bd72c25fdb102b
kntran <kntran@kntran-lap1.oradev.oraclecorp.com>
2020-04-26 14:54:51 -0700

    update euroka connection info

--- Lab.DiscoveryService/src/main/java/lab/security/WebSecurity.java
+++ Lab.DiscoveryService/src/main/java/lab/security/WebSecurity.java
@@ -1,19 +1,35 @@
 package lab.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		//http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+        .and().requestMatchers().antMatchers("/eureka/**")
+        .and().authorizeRequests().antMatchers("/eureka/**")
+        .hasRole("SYSTEM").anyRequest().denyAll().and()
+        .httpBasic().and().csrf().disable();
 	}
-	
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("welcome123").roles("SYSTEM");
+	}
+
 //	@Bean
 //	@Override
 //	public UserDetailsService userDetailsService() {
