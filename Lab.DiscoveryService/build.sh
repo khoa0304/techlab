9fa15268f1b7964512f4f752c126f19756f79d25
kntran <kntran@kntran-lap1.oradev.oraclecorp.com>
2020-05-10 23:59:58 -0700

    update build steps

--- Lab.DiscoveryService/build.sh
+++ Lab.DiscoveryService/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 git pull
+
+cd ../Lab.ConfigService
+pwd
+mvn clean package spring-boot:repackage
+
+cd Lab.DiscoveryService
+pwd
 mvn clean package spring-boot:repackage
 docker-compose up --build -d
--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 git pull
+
+cd Lab.Common
+pwd
+mvn clean install
+
+cd ..
+pwd
 mvn clean package spring-boot:repackage
 docker-compose up --build 
